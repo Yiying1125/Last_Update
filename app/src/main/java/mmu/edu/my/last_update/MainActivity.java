51f8986d87cfea6e922b6c67e48f54abf19d5964
Yiying1125 <75288823+Yiying1125@users.noreply.github.com>
2021-01-01 18:11:54 +0800

    update api data

--- app/src/main/java/mmu/edu/my/last_update/MainActivity.java
+++ app/src/main/java/mmu/edu/my/last_update/MainActivity.java
@@ -1,28 +1,25 @@
 package mmu.edu.my.last_update;
 
 
-import androidx.annotation.RequiresApi;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 
 import android.os.Handler;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -33,28 +30,30 @@ public class MainActivity extends AppCompatActivity {
     private TextView tv_totalnumber1, tv_activenumber1, tv_deathnumber1, tv_recoverednumber1, tv_todaynumber1;
     private String str_total, str_active, str_death, str_recovered, str_today, str_tadaycal;
     private SwipeRefreshLayout swipeRefreshLayout;
-
     private int int_today;
     private ProgressDialog progressDialog;
+    private String appUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.latest_update);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        getSupportActionBar().setTitle("TraCo-19 (Latest Update)");
         //initialise
         Init();
         //Fetch data from API
         FetchData();
-
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 FetchData();
                 swipeRefreshLayout.setRefreshing(false);
-                //Toast.makeText(MainActivity.this, "Data refreshed!", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
     private void FetchData() {
@@ -70,14 +69,13 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            JSONObject result = new JSONObject(apiUrl);
-                            str_total = result.getString("testedPositive");
-                            str_active = result.getString("activeCases");
-                            str_death = result.getString("deceased");
-                            str_recovered = result.getString("recovered");
+                            str_total = response.getString("testedPositive");
+                            str_active = response.getString("activeCases");
+                            str_death = response.getString("deceased");
+                            str_recovered = response.getString("recovered");
 
-                            Handler delayToShowProgess = new Handler();
-                            delayToShowProgess.postDelayed(new Runnable() {
+                            Handler delayToShowProgress = new Handler();
+                            delayToShowProgress.postDelayed(new Runnable() {
                                 @Override
                                 public void run() {
                                     //Setting text in the textview
@@ -85,43 +83,44 @@ public class MainActivity extends AppCompatActivity {
                                     tv_activenumber1.setText(NumberFormat.getInstance().format(Integer.parseInt(str_active)));
                                     tv_recoverednumber1.setText(NumberFormat.getInstance().format(Integer.parseInt(str_recovered)));
                                     tv_deathnumber1.setText(NumberFormat.getInstance().format(Integer.parseInt(str_death)));
-                                    int_today = Integer.parseInt(str_total)
-                                            - (Integer.parseInt(str_total) + Integer.parseInt(str_death));
+                                    /*int_today = Integer.parseInt(str_total)
+                                            - (Integer.parseInt(str_total) + Integer.parseInt(str_death));*/
                                     tv_todaynumber1.setText(NumberFormat.getInstance().format(int_today));
-                                DismissDialog();
-                                }
-                            }, 1000);
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
+                                    dismissDialog();
+                                }}, 1000);
+                        } catch (JSONException e) { e.printStackTrace(); }
+                    }},
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                    }
-                });
+                        error.printStackTrace();
+                    } }
+                        );
         requestQueue.add(jsonObjectRequest);
     }
     public void ShowDialog(Context context) {
         //setting up progress dialog
-        progressDialog = new ProgressDialog(context);
+        progressDialog = new ProgressDialog(this);
         progressDialog.show();
         progressDialog.setContentView(R.layout.progress_dialog);
         progressDialog.setCanceledOnTouchOutside(false);
         progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
     }
-    public void DismissDialog() {
-        progressDialog.dismiss();
-    }
+    public void dismissDialog(){progressDialog.dismiss();}
     private void Init() {
         tv_totalnumber1 = findViewById(R.id.totalnumber2);
         tv_activenumber1 = findViewById(R.id.activenumber2);
         tv_deathnumber1= findViewById(R.id.deathnumber2);
         tv_recoverednumber1= findViewById(R.id.recoverednumber2);
         tv_todaynumber1 = findViewById(R.id.todaynumber2);
-        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout=findViewById(R.id.swipe_refresh_layout);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
     }
 }
 
